68e10b3fd8abdf2f06024ad274c3598260e1e3fe
pabrrs <paulo.alfredo.barros@gmail.com>
2020-08-27 14:03:58 -0300

    :sparkles: Add all components inside main tag in Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="w-3/4 mx-auto">
-    <AppHeader />
-    <HeroSection />
-  </div>
+  <AppHeader :class="defaultContainer" />
+  <main>
+    <HeroSection :class="`${defaultContainer} ${defaultSectionSpacing}`" />
+    <ServicesSection :class="`${defaultContainer} ${defaultSectionSpacing}`" />
+  </main>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue'
 import HeroSection from '@/components/sections/Hero.vue'
+import ServicesSection from '@/components/sections/Services.vue'
+
 export default {
   name: 'Home',
+  data: function() {
+    return {
+      defaultContainer: 'w-2/3 mx-auto',
+      defaultSectionSpacing: 'mb-32'
+    }
+  },
   components: {
     AppHeader,
-    HeroSection
+    HeroSection,
+    ServicesSection
   }
 }
 </script>
