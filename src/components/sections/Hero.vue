89e87c6e39709cdb2f77cf05603b5d6d998e0e90
pabrrs <paulo.alfredo.barros@gmail.com>
2020-08-27 14:03:26 -0300

    :sparkles: Add hero section

--- src/components/sections/Hero.vue
+++ src/components/sections/Hero.vue
@@ -1,33 +1,35 @@
 <template>
   <section>
-    <div class="grid grid-cols-2">
-      <article class="mt-24">
+    <div class="grid md:grid-cols-2 grid-cols-1">
+      <div class="mt-12 md:mt-24">
         <h1 class="font-bold text-5.5xl leading-tight my-5">
           Você no controle
-          <br />da sua carreira!
+          <br class="invisible md:visible" />da sua carreira!
         </h1>
-        <h2 class="font-light text-lg text-gray-600 my-3 leading-tight">
+        <p class="font-light text-lg text-gray-600 my-3 leading-tight">
           Empodere-se da sua jornada e seja destaque
-          <br />nos processos seletivos com a elevatte
-        </h2>
+          <br class="invisible md:visible" />nos processos seletivos com a elevatte
+        </p>
         <div class="my-20">
-          <a href="#book-mentorship" class="rounded-full bg-main text-white py-4 px-10 text-base">Agendar mentoria</a>
+          <a href="#book-mentorship" class="rounded-full bg-primary text-white py-4 px-10 text-base w-full lg:w-auto"
+            >Agendar mentoria</a
+          >
         </div>
-      </article>
-      <figure>
-        <img class="float-right w-3/4 h-auto" :src="heroIllustration" alt="Mulher sentada com notebook" />
+      </div>
+      <figure class="invisible md:visible">
+        <img class="float-right w-4/5 h-auto" :src="heroArt" alt="Mulher sentada com notebook" />
       </figure>
     </div>
   </section>
 </template>
 
 <script>
-import heroIllustration from '@/assets/img/hero-illustration.svg'
+import heroArt from '@/assets/img/hero-art.svg'
 export default {
   name: 'Hero',
   data: function() {
     return {
-      heroIllustration
+      heroArt
     }
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
         '5.5xl': '3.5rem'
       },
       backgroundColor: {
-        main: '#3271FC'
+        primary: '#3271FC'
+      },
+      borderColor: {
+        primary: '#3271FC'
+      },
+      borderRadius: {
+        xlg: '1.3rem'
+      },
+      boxShadow: {
+        linear: '0 0 25px rgba(0, 0, 0, 0.05), 0 2px 3px -1px rgba(0, 0, 0, 0.05);'
       }
     }
   },
