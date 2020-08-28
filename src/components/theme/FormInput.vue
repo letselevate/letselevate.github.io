f566617fe7869951cf297270f7f6641d0defbbec
pabrrs <paulo.alfredo.barros@gmail.com>
2020-08-28 02:02:49 -0300

    :construction_worker: Draft FormInput

--- src/components/theme/FormInput.vue
+++ src/components/theme/FormInput.vue
@@ -4,7 +4,7 @@
       :type="type"
       :placeholder="placeholder"
       :required="required"
-      class="border-2 border-primary bg-blue-soft p-4 rounded-lg placeholder-blue-500"
+      class="border-2 border-primary bg-blue-light p-4 rounded-lg placeholder-blue-500"
     />
   </fieldset>
 </template>
