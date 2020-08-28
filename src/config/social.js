02a0b07bed9a95f79df9b71a5a08a45ec1b08104
pabrrs <paulo.alfredo.barros@gmail.com>
2020-08-28 11:17:25 -0300

    :bug: Fix instagram url

--- src/config/social.js
+++ src/config/social.js
@@ -1,8 +1,8 @@
 const {
   ELVT_SOCIAL_EMAIL = 'elevatte@letselevate.com.br',
-  ELVT_SOCIAL_LINKEDIN = 'https://linkedin.com/company/elevatte.me',
+  ELVT_SOCIAL_LINKEDIN = 'https://linkedin.com/company/elevatte',
   ELVT_SOCIAL_GITHUB = 'https://github.com/elevatte',
-  ELVT_SOCIAL_INSTAGRAM = 'https://instagram.com/elevatte.me'
+  ELVT_SOCIAL_INSTAGRAM = 'https://instagram.com/elevatte.app'
 } = process.env
 
 export default {
